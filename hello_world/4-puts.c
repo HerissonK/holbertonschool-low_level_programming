fbcb00ff8d21fe8068d621ba283452a0732e44d9
HerissonK <11513@Holbertonstudents.com>
2025-06-16 10:00:12 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,9 @@
-/**
- *  File: 4-puts.c.
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle"
+ * main - Prints Programming is like building a multilingual puzzle
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
