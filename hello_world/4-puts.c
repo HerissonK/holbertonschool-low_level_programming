bdb1741f6514ea87a2c81c295da5f52a7f55c521
HerissonK <11513@Holbertonstudents.com>
2025-06-16 09:22:49 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle
+ * Main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
