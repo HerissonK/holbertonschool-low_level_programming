20c76ae1fb6c126d2cf2506180d9bb575ff515b3
HerissonK <11513@Holbertonstudents.com>
2025-06-18 11:36:52 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,21 +3,25 @@
 /**
   * print_sign - Checks for lowercase character
   *
-  * @c: character to be tested
+  * @n: character to be tested
   * Return: 1 for positive 0 for zero and -1 for negative
   */
 
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar(43);
-	return(1);
-
+	return (1);
+	}
 	else if (n < 0)
+	{
 		_putchar(45);
-	return(-1);
-
+	return (-1);
+	}
 	else
-		_putchar(48)
-	return(0)
+	{
+		_putchar(48);
+	return (0);
+		}
 }
