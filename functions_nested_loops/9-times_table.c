70955d790fb142a1189d841d5b0ae43614ca574b
HerissonK <11513@Holbertonstudents.com>
2025-06-20 09:17:16 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,19 +8,37 @@
 
 void times_table(void)
 {
-	int n, f;
+	int x, y, z, u, d;
 
-	n = 0;
-
-	while (n < 9)
+	for (x = 0; x <= 9; x++)
 	{
-		f = 0;
-		while (f < 9)
+		for (y = 0; y <= 9; y++)
 		{
-			_putchar(n * f);
-				f++;
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
 		}
+
 		_putchar('\n');
-		n++;
 	}
 }
