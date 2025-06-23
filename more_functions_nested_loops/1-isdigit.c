08c6114c994076b27ab03e66aab8559d1fd4c920
HerissonK <11513@Holbertonstudents.com>
2025-06-23 07:56:25 +0000

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for alphabetic character
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 for numeric character or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
