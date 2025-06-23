fb8335d1146a7289b58b16d62fbb79d0c22953ed
HerissonK <11513@Holbertonstudents.com>
2025-06-23 09:08:23 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,13 @@ void more_numbers(void)
 	for (d = 0; d < 10; d++)
 	{
 		for (u = 0; u <= 14; u++)
+		{
 			if (u > 9)
+			{
 				_putchar((u / 10) + '0');
-		_putchar((u % 10) + '0');
+			}
+			_putchar((u % 10) + '0');
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
