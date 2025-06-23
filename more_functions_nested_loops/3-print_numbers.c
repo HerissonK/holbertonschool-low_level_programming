2565eb4236aed19258943b066451cf3fcf24abe8
HerissonK <11513@Holbertonstudents.com>
2025-06-23 08:11:06 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: prints "_putchar" followed by a new line using _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '1'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
