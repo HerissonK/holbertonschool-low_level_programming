02faadf61bb3a77723eba1ab3d2e7b51d6ef4b51
HerissonK <11513@Holbertonstudents.com>
2025-06-24 13:20:21 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - returns the length of a string
+ *
+ * @dest: where to write
+ * @src: where to get the string
+ *
+ * Return: returns lenght;
+ */
+
+int _atoi(char *s)
+	
