e36bc499115d2165460e2e1fce45b94031562014
HerissonK <11513@Holbertonstudents.com>
2025-06-25 09:15:00 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * _strcat - copy the src after dest
+ * _strncat - copy the src after dest
  *
  * @dest: where to write
  * @src: where to get the string
+ * @n: numbers to print
  *
  * Return: returns lenght;
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
 
 	for (b = 0; dest[b] != '\0'; b++)
 	;
-	for (a = 0; src[a] != '\0'; i++)
+	for (a = 0; src[a] != '\0' && a < n; a++)
 	{
 		dest[a + b] = src[a];
 	}
