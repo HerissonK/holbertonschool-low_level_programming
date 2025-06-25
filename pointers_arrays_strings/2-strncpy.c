9d6d69be9c2dad731ed6dd1cf36ff83870332956
HerissonK <11513@Holbertonstudents.com>
2025-06-25 09:53:15 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - returns the length of a string
+ *
+ * @dest: where to write
+ * @src: where to get the string
+ * @n: number of character to copy
+ *
+ * Return: the copy;
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int str;
+
+	for (str = 0; src[str] != '\0' && str < n; str++)
+	{
+		dest[str] = src[i];
+	}
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
