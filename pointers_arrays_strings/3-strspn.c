6a0d54adbea1d9ed0cab37a50db74a042fe08317
HerissonK <11513@Holbertonstudents.com>
2025-06-30 12:05:47 +0000

    3-strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: the string to verify
+ * @accept: the characters to find in the sring
+ *
+ * Return: return the character if not in the leet array
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int byte = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+			{
+				byte++;
+			}
+			else if ((accept[i + 1]) == '\0')
+				return (byte);
+		}
+		s++;
+	}
+	return (byte);
+}
