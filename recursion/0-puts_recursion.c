8482632763d90ee9c8435e76279b97f0da05aba0
HerissonK <11513@Holbertonstudents.com>
2025-07-01 08:24:57 +0000

    0-puts_recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of the 2 diagonals
+ *
+ * @a: array to find numbers
+ * @size: the size of the array
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
