6ad0b311b1955caf5da5cdba00dce9847f55fb02
HerissonK <11513@Holbertonstudents.com>
2025-07-02 08:05:02 +0000

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - get the name of the program
+ *
+ * @argc: number of argument
+ * @argv: array of argument
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int somme;
+
+	
