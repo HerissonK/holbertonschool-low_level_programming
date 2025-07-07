c9d448a2a0035eb3c19c75ede7cbc36e21094108
HerissonK <11513@Holbertonstudents.com>
2025-07-07 17:00:51 +0000

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,39 +21,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (len1 = 0; *(s1 + len1) != '\0'; len1++)
 	{
 		;
 	}
-
 	for (len2 = 0; *(s2 + len2) != '\0'; len2++)
 	{
 		;
 	}
-
 	if (n >= len2)
 	{
 		n = len2;
 	}
-
 	str = malloc(sizeof(char) * (len1 + n + 1));
-
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len1; i++)
 	{
 		str[i] = s1[i];
 	}
-
 	for (j = 0; j < n; i++, j++)
 	{
 		str[i] = s2[j];
