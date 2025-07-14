8c6e4b441d4a04fd9b402fad08d2ae53bbddba1e
HerissonK <11513@Holbertonstudents.com>
2025-07-14 13:28:41 +0000

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that prints a name
+ *
+ * @array: the element
+ * @size: of the array
+ * @cmp: the function that compares values
+ *
+ * Return: return -1 for no value or the values
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
