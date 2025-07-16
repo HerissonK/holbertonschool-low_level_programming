2212c89c4bd265f28beb3c96802db0f5c47e5d3f
HerissonK <11513@Holbertonstudents.com>
2025-07-16 14:34:46 +0000

    premier essai sans betty

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,16 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+typedef struct
+{
+	void (*f)(va_list);
+	char spec;
+} array;
 
 #endif
