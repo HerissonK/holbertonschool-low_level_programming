c80a64665e870fe98864c77c0284d805c7d555cb
HerissonK <11513@Holbertonstudents.com>
2025-07-18 09:39:44 +0000

    100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,15 @@
 section .data
-    msg db "Hello, World", 10
-    len equ $ - msg
+	msg db "Hello, World"
+	len equ $ -msg
 
 section .text
-    global _start
+	global _start
 
 _start:
-    mov rax, 1          ; syscall: write
-    mov rdi, 1          ; file descriptor: stdout
-    mov rsi, msg        ; pointer to message
-    mov rdx, len        ; message length
-    syscall             ; perform write
-
-    ; Exit cleanly
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; exit code 0
-    syscall
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+	mov eax, 1
+	int 0x80
