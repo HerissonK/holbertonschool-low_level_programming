754019c2c5433da4e79b5cd5313298ca5f60e7c0
HerissonK <11513@Holbertonstudents.com>
2025-07-29 09:09:26 +0000

    1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -3,20 +3,19 @@
 #include <stdio.h>
 
 /**
- * print_list - print all the element from a list "list_t"
+ * print_dlistint - print all the element from a list "list_t"
  *
  * @h: current node
  *
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
