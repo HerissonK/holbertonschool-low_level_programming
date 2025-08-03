083611b69a508c8044aacaa90fd648db491b2bae
HerissonK <11513@Holbertonstudents.com>
2025-08-03 18:42:29 +0000

    100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -2,15 +2,16 @@
 #include <string.h>
 #include <unistd.h>
 
+
+void myStartupFun(void) __attribute__ ((constructor));
+
 /**
  * myStartupFun - function to run command before main function
  * Return: always successful
  */
 
-void myStartupFun(void) __attribute__ ((constructor));
-
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
